31bb4ddb5362dbee99d490a47ae1c6a3da2d38ce
GitJoeButt <jbkell2@yahoo.com>
2021-01-13 08:22:36 -0600

    Populating dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -2,5 +2,19 @@ var data = d3.json("./data/samples.json").then(function(incomingData) {
     console.log(incomingData.names);
     console.log(samples[0].id);
     console.log(samples[1].id);
+    
+    //Populate the dropdown
+    d3.select("#selDataset")
+    .selectAll("option")
+    .data(incomingData.samples)
+    .enter()
+    .append("option")
+    .text(function(d) {
+      return d.id;
+    })
+    .property("value",function(d) {
+        return d.id;
+      });
+
     return incomingData;
 });
